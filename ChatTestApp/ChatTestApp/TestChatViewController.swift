f0184903f97a97fa5f3262dc2b6bf445bd5f6eaa
Leanid Hubarenka <Leanid.Hubarenka@itechart-group.com>
2020-06-24 10:39:59 +0300

    Delete unused code

--- ChatTestApp/ChatTestApp/TestChatViewController.swift
+++ ChatTestApp/ChatTestApp/TestChatViewController.swift
@@ -16,16 +16,7 @@ class TestChatViewController: MessagesViewController {
     private struct Constants {
         static let thread = "thread1"
     }
-    
-    lazy var attachmentManager: AttachmentManager = { [unowned self] in
-         let manager = AttachmentManager()
-        manager.isPersistent = true
-        manager.showAddAttachmentCell = true
-         manager.delegate = self
-        manager.dataSource = self
-         return manager
-     }()
-    
+
     var messages: [Message] = []
     let reference = Database.database().reference()
     private let user: User
@@ -116,78 +107,4 @@ extension TestChatViewController: MessagesDataSource {
     }
 }
 
-extension TestChatViewController: MessagesDisplayDelegate, MessagesLayoutDelegate {
-  
-}
-
-extension TestChatViewController: AttachmentManagerDelegate, AttachmentManagerDataSource {
-    func attachmentManager(_ manager: AttachmentManager, cellFor attachment: AttachmentManager.Attachment, at index: Int) -> AttachmentCell {
-        return AttachmentCell()
-    }
-    
-    // MARK: - AttachmentManagerDelegate
-    
-    func attachmentManager(_ manager: AttachmentManager, shouldBecomeVisible: Bool) {
-        setAttachmentManager(active: shouldBecomeVisible)
-    }
-    
-    func attachmentManager(_ manager: AttachmentManager, didReloadTo attachments: [AttachmentManager.Attachment]) {
-        messageInputBar.sendButton.isEnabled = !manager.attachments.isEmpty
-    }
-    
-    func attachmentManager(_ manager: AttachmentManager, didInsert attachment: AttachmentManager.Attachment, at index: Int) {
-        messageInputBar.sendButton.isEnabled = !manager.attachments.isEmpty
-    }
-    
-    func attachmentManager(_ manager: AttachmentManager, didRemove attachment: AttachmentManager.Attachment, at index: Int) {
-        messageInputBar.sendButton.isEnabled = !manager.attachments.isEmpty
-    }
-    
-    func attachmentManager(_ manager: AttachmentManager, didSelectAddAttachmentAt index: Int) {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
-        present(imagePicker, animated: true, completion: nil)
-    }
-    
-    // MARK: - AttachmentManagerDelegate Helper
-    
-    func setAttachmentManager(active: Bool) {
-        
-        let topStackView = messageInputBar.topStackView
-        if active && !topStackView.arrangedSubviews.contains(attachmentManager.attachmentView) {
-            topStackView.insertArrangedSubview(attachmentManager.attachmentView, at: topStackView.arrangedSubviews.count)
-            topStackView.layoutIfNeeded()
-        } else if !active && topStackView.arrangedSubviews.contains(attachmentManager.attachmentView) {
-            topStackView.removeArrangedSubview(attachmentManager.attachmentView)
-            topStackView.layoutIfNeeded()
-        }
-    }
-}
-
-extension TestChatViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        // Local variable inserted by Swift 4.2 migrator.
-        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
-        
-        dismiss(animated: true, completion: {
-            if let pickedImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
-                let handled = self.attachmentManager.handleInput(of: pickedImage)
-                if !handled {
-                    // throw error
-                }
-            }
-        })
-    }
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-private func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-    return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-private func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-    return input.rawValue
-}
+extension TestChatViewController: MessagesDisplayDelegate, MessagesLayoutDelegate {}
